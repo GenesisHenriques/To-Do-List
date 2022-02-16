3c565aff9e380db1d4791f77cd0e5cbe0a6dddb0
Genesis-Henriques <GenesisHenriques@gmail.com>
2022-02-15 21:55:10 -0300

    Rota removeTask pronta

--- BackEnd/Controllers/Tasks/router.js
+++ BackEnd/Controllers/Tasks/router.js
@@ -3,11 +3,13 @@ const express = require('express');
 const getAllTasks = require('./getAllTasks');
 const createTask = require('./createTask');
 const getTaskById = require('./getTaskById');
+const removeTask = require('./removeTask');
 
 const router = express.Router({ mergeParams: true });
 
 router.get('/', getAllTasks);
 router.get('/:id', getTaskById);
 router.post('/', createTask);
+router.delete('/:id', removeTask);
 
 module.exports = router;
